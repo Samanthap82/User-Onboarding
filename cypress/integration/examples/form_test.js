34609a11dc94a197a42207f80f3daa26bcc6bc6f
Samantha Prewitt <67523846+Samanthap82@users.noreply.github.com>
2020-11-12 17:47:44 -0600

    cypress project

---
+++ cypress/integration/examples/form_test.js
@@ -0,0 +1,49 @@
+describe('User Onboarding Project Page', () => {
+    beforeEach(()=>{
+        cy.visit('http://localhost:3000')
+    })
+    
+    const nameInput = () => cy.get('input[type=text]');
+    const emailInput = () => cy.get('input[type=email]')
+    const passwordInput = () => cy.get('input[type=password]')
+    const checkboxInput = () => cy.get('input[type=checkbox]');
+    const submit = () => 
+        cy.get('button')
+    
+
+    it('testing', () => {
+        nameInput().should('exist')
+        emailInput().should('exist')
+        passwordInput().should('exist')
+        checkboxInput().should('exist')
+    })
+
+    it('testing inputs', () => {
+        nameInput().type('Samantha Prewitt').should('have.value', 'Samantha Prewitt');
+
+        emailInput().type('samanthaprewitt@gmail.com').should('have.value', 'samanthaprewitt@gmail.com');
+
+        passwordInput().type('password').should('have.value', 'password')
+
+        checkboxInput().check();
+
+        submit().click();
+
+        nameInput().should('not.have.value', '')
+
+        emailInput().should('not.have.value', '')
+
+        passwordInput().should('not.have.value', '')
+
+        checkboxInput().should('not.have.value', 'false')
+
+    })
+    
+    it('checking for validation', () => {
+        passwordInput()
+        .type('ccccccc')
+        cy.contains('Password must be at least 8 characters long').should('exist')
+        passwordInput().clear();
+    });
+
+})
--- onboardingapp/src/App.js
+++ onboardingapp/src/App.js
@@ -1,38 +1,40 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import Form from "./Form";
-import schema from "./formSchema";
-import axios from "axios";
-import * as yup from "yup";
-import styled from "styled-components";
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import Form from './Form';
+import schema from './formSchema';
+import axios from 'axios';
+import * as yup from 'yup';
+import styled from 'styled-components';
+
 
 const H1 = styled.h1`
-  font-size: 3rem;
-  text-align: center;
-`;
+font-size: 3rem;
+text-align: center;
+`
 
 const Pre = styled.pre`
-  display: block;
-  width: 40%;
-  margin: auto;
-`;
+display: block;
+width: 40%;
+margin: auto;
+`
 
 const initialFormValues = {
-  name: "",
-  email: "",
-  password: "",
-  terms: "",
-};
+  name: '',
+  email: '',
+  password: '',
+  terms: '',
+}
 
 const initialFormErrors = {
-  name: "",
-  email: "",
-  password: "",
-  terms: "",
-};
+  name: '',
+  email: '',
+  password: '',
+  terms: ''
+}
 
 const initialPeople = [];
 const initialDisabled = true;
+
 
 function App() {
   /*setting state */
@@ -43,85 +45,81 @@ function App() {
 
   /* functions */
 
-  const postNewPerson = (newPerson) => {
-    axios
-      .post("https://reqres.in/api/users")
-      .then((res) => {
-        setPeople([...people, { id: res.data.id, ...newPerson }]);
-        setFormValues(initialFormValues);
-      })
-      .catch((err) => {
-        alert(err);
-      });
-  };
+  
+  const postNewPerson = newPerson => {
+    axios.post('https://reqres.in/api/users')
+    .then(res=>{
+      setPeople([...people, {id: res.data.id,...newPerson}])
+      setFormValues(initialFormValues);
+    })
+    .catch(err=>{
+      alert(err)
+    })
+  }
 
   const validate = (name, value) => {
     yup
-      .reach(schema, name)
-      .validate(value)
-      .then((valid) => {
-        setFormErrors({
-          ...formErrors,
-          [name]: "",
-        });
+    .reach(schema, name)
+    .validate(value)
+    .then(valid=>{
+      setFormErrors({
+        ...formErrors,
+        [name]: ""
       })
-      .catch((err) => {
-        setFormErrors({
-          ...formErrors,
-          [name]: err.errors[0],
-        });
+    })
+    .catch(err=>{
+      setFormErrors({
+        ...formErrors,
+        [name]: err.errors[0]
       });
-  };
+    });
+  }
 
   /* event handlers */
   const inputChange = (name, value) => {
     validate(name, value);
     setFormValues({
       ...formValues,
-      [name]: value,
-    });
-  };
+      [name]: value
+    })
+  }
 
   const formSubmit = () => {
     const newPerson = {
       name: formValues.name.trim(),
       email: formValues.email.trim(),
       password: formValues.password.trim(),
-      terms: true,
-    };
-    postNewPerson(newPerson);
-  };
+      terms: true
+    }
+    postNewPerson(newPerson)
+  }
 
-  useEffect(() => {
-    axios
-      .get("https://reqres.in/api/users")
-      .then((res) => {
-        setPeople(res.data.data);
-      })
-      .catch((err) => {
-        alert(err);
-      });
+  useEffect(()=>{
+    axios.get('https://reqres.in/api/users')
+    .then(res=>{
+      setPeople(res.data.data)
+    })
+    .catch(err=>{
+      alert(err);
+    })
   }, []);
 
   /*seeing if 'disabled' needs to be adjusted when values are changed */
-  useEffect(() => {
-    schema.isValid(formValues).then((valid) => {
+  useEffect(()=>{
+    schema.isValid(formValues)
+    .then(valid=>{
       setDisabled(!valid);
-    });
-  }, [formValues]);
+    })
+  }, [formValues])
   return (
     <div>
       <H1>Users List/Info</H1>
-      {people.map((person) => {
-        return <Pre>{JSON.stringify(person, null, 4)}</Pre>;
+      {people.map(person=>{
+        return (
+          <Pre>{JSON.stringify(person, null, 4)}</Pre>
+        )
       })}
-      <Form
-        values={formValues}
-        submit={formSubmit}
-        change={inputChange}
-        disabled={disabled}
-        errors={formErrors}
-      />
+      <Form values={formValues} submit={formSubmit} change={inputChange} disabled={disabled} errors={formErrors}/>
     </div>
   );
 }
